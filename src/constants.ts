27e88ae2a405a9b6e3e8c62ff41b221c944ad66f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-10-31 17:59:57 +0000

    Fix the height of the icon, when viewed in Obsidian Hub Vault

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 // From https://icon-sets.iconify.design/feather/crosshair/ - "feather-crosshair"
 export const MANUALLY_REVEAL_ACTIVE_FILE_ICON =
-  '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="100" height="100" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><path d="M22 12h-4"/><path d="M6 12H2"/><path d="M12 6V2"/><path d="M12 22v-4"/></g></svg>';
+  '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="18" height="18" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><path d="M22 12h-4"/><path d="M6 12H2"/><path d="M12 6V2"/><path d="M12 22v-4"/></g></svg>';

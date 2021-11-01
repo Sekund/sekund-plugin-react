dc23a1a8f297a0623a0cfc8115c7e337f9316adc
Candide Kemmler <hi@candide.me>
2021-11-01 12:54:42 +0100

    moving stylesheets to styles dir

--- src/ui/SekundComponent.stories.tsx
+++ src/ui/SekundComponent.stories.tsx
@@ -3,6 +3,7 @@ import withConnectionStatus from '@/ui/withConnectionStatus';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { TFile } from 'obsidian';
 import React from 'react';
+import '@/styles/obsidian-app.css'
 import '/global.css';
 
 function AllGood() {

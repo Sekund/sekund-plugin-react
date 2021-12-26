62e78737026138b3d14c8093e1ddbe82ee32a34a
Candide Kemmler <hi@candide.me>
2021-12-26 15:15:29 +0100

    add preview mode for main comment area

--- src/ui/note/NoteComments.tsx
+++ src/ui/note/NoteComments.tsx
@@ -8,6 +8,7 @@ import { AppActionKind } from "@/state/AppReducer";
 import GlobalState from "@/state/GlobalState";
 import NoteCommentComponent from "@/ui/note/NoteCommentComponent";
 import { dispatch, makeid } from "@/utils";
+import Markdown from "markdown-to-jsx";
 import React, { Fragment, useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -19,6 +20,8 @@ export default function NoteComments({ note }: Props) {
   const { appState, appDispatch } = useAppContext();
   const { t } = useTranslation(["common", "plugin"]);
   const { userProfile, plugin, remoteNote } = appState;
+
+  const [preview, setPreview] = useState(false);
 
   const sendButton = useRef<HTMLButtonElement>(null);
   const [areaText, setAreaText] = useState("");
@@ -136,44 +139,77 @@ export default function NoteComments({ note }: Props) {
     }
   }
 
+  function togglePreview() {
+    if (preview) {
+      setPreview(!preview);
+      setTimeout(() => {
+        const textarea = document.getElementById("sekund-comment") as HTMLTextAreaElement;
+        if (textarea.parentNode) {
+          (textarea.parentNode as HTMLElement).dataset.replicatedValue = areaText;
+          textarea.value = areaText;
+        }
+      }, 10);
+    } else {
+      const textarea = document.getElementById("sekund-comment") as HTMLTextAreaElement;
+      if (textarea.parentNode) {
+        setAreaText(textarea.value);
+        (textarea.parentNode as HTMLElement).dataset.replicatedValue = "";
+      }
+      setPreview(!preview);
+    }
+  }
+
   return (
     <div className="px-2 mt-1 mb-16">
       <div className={`sm:col-span-2`}>
-        <label htmlFor="message" className="flex items-center h-10 space-x-2 text-obs-muted">
-          <span>{peopleAvatar(userProfile, 8)}</span> <span>{t("you")}</span>
-        </label>
-        <div className="mt-1 grow-wrap">
-          <textarea
-            onInput={autoexpand}
-            onChange={(e) => setAreaText(e.target.value)}
-            onKeyPress={handleKeyPress}
-            id="sekund-comment"
-            name="message"
-            rows={2}
-            spellCheck="false"
-            className="relative block w-full px-2 py-1 text-sm border rounded-md"
-            aria-describedby="message-max"
-            defaultValue={""}
-          />
+        <div className="flex items-center justify-between">
+          <label htmlFor="message" className="flex items-center h-10 space-x-2 text-obs-muted">
+            <span>{peopleAvatar(userProfile, 8)}</span> <span>{t("you")}</span>
+          </label>
+          <button className={`mr-0 ${areaText === "" ? "text-obs-faint" : "text-obs-normal"}`} onClick={togglePreview}>
+            {preview ? t("edit") : t("preview")}
+          </button>
         </div>
+        {preview ? (
+          <div className="mt-1 px-2 pt-2">
+            <Markdown>{areaText}</Markdown>
+          </div>
+        ) : (
+          <div className="mt-1 grow-wrap">
+            <textarea
+              onInput={autoexpand}
+              onChange={(e) => setAreaText(e.target.value)}
+              onKeyPress={handleKeyPress}
+              id="sekund-comment"
+              name="message"
+              rows={2}
+              spellCheck="false"
+              className="relative block w-full px-2 py-1 text-sm border rounded-md"
+              aria-describedby="message-max"
+              defaultValue={""}
+            />
+          </div>
+        )}
       </div>
-      <div className="flex justify-end w-full mt-2">
-        <button
-          className={`mr-2 ${areaText === "" ? "text-obs-faint" : "text-obs-normal"}`}
-          onClick={areaText === "" ? undefined : clearComment}
-          type="button"
-        >
-          {t("clear")}
-        </button>
-        <button
-          className={`mr-0 ${areaText === "" ? "text-obs-faint" : "text-obs-normal mod-cta"}`}
-          ref={sendButton}
-          onClick={areaText === "" ? undefined : addComment}
-          type="button"
-        >
-          {t("send")}
-        </button>
-      </div>
+      {preview ? null : (
+        <div className="flex justify-end w-full mt-2">
+          <button
+            className={`mr-2 ${areaText === "" ? "text-obs-faint" : "text-obs-normal"}`}
+            onClick={areaText === "" ? undefined : clearComment}
+            type="button"
+          >
+            {t("clear")}
+          </button>
+          <button
+            className={`mr-0 ${areaText === "" ? "text-obs-faint" : "text-obs-normal mod-cta"}`}
+            ref={sendButton}
+            onClick={areaText === "" ? undefined : addComment}
+            type="button"
+          >
+            {t("send")}
+          </button>
+        </div>
+      )}
       <div className="flex flex-col mt-4 space-y-4">
         {localComments
           ?.sort((a, b) => (a.created > b.created ? -1 : 1))

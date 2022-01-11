45bc65eb9337c0ec251b2e5e8e50ffe34808b296
Candide Kemmler <hi@candide.me>
2022-01-11 09:56:06 +0100

    fix api key generation

--- src/services/ApiKeyService.ts
+++ src/services/ApiKeyService.ts
@@ -2,7 +2,7 @@ export default class ApiKeyService {
   constructor(private user: Realm.User) {}
 
   async ensureApiKey(vaultName: string) {
-    function hashCode(str) {
+    function hashCode(str: string) {
       var hash = 0,
         i = 0,
         len = str.length;
@@ -12,6 +12,12 @@ export default class ApiKeyService {
       return hash;
     }
 
+    // always re-create the api key associated with this vault
+    const allKeys = await this.user.apiKeys.fetchAll();
+    const vaultKeys = allKeys.filter((k) => k.name === "apiKey_" + hashCode(vaultName));
+    if (vaultKeys.length > 0) {
+      await this.user.apiKeys.delete(vaultKeys[0]._id);
+    }
     return await this.user.apiKeys.create("apiKey_" + hashCode(vaultName));
   }
 }

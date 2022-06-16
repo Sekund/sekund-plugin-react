f5b911a5e562150aa952fd9b4258d55ac4b2f096
Candide Kemmler <hi@candide.me>
2022-06-16 11:12:46 +0200

    release & version bump to 1.0.60

--- src/main.tsx
+++ src/main.tsx
@@ -430,7 +430,7 @@ class AboutModal extends Modal {
       <p style="font-size: 85%">Candide Kemmler</p>
       <p><b>${i18next.t("contributors")}</b></p>
       <p style="font-size: 85%">Laurent De Saedeleer (UX)</p>
-      <p style="margin-top:1rem">Version: 1.0.59</p>
+      <p style="margin-top:1rem">Version: 1.0.60</p>
     </div>
     `;
   }
